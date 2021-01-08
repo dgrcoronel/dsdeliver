3f9967ef4e10f59eca4a8dc6ae0017ffca0653b7
Daniela Coronel <daniela.gonzaga@gmail.com>
2021-01-08 18:15:26 -0300

    Add navibar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import './Hello';
-import Hello from './Hello';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <Hello />
+      <Navbar />
     </div>
   );
 }
